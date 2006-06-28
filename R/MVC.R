757611e061ee212d89a9e6e88e7fd0847d7aca9d
Whalen <whalen@jimmy.harvard.edu>
2006-06-28 20:07:41 +0000

    Added gene set enrichment classes.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BioMVCClass@18646 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/MVC.R
+++ R/MVC.R
@@ -1,4 +1,96 @@
+#########
+# create a class to hold gene set enrichment data
+#########
+# not sure if I need to store the exprSet data
+# incidMat is the incidence matrix that shows the relationships between
+#  genes and gene sets
+# gTestStat is the test statistic for the gene with the phenotype
+# gsTestStat is the test statistic for the gene set that is obtained by matrix
+#  multiplication of incidMat and gTestStat
+# expData is the experimental data (here an exprSet)
+# descr is for a description of the gene set being studied
+setClass("GSE", representation(incidMat="matrix", gTestStat="numeric",
+                 gsTestStat="numeric", expData="exprSet", descr="character"))
 
+if (is.null(getGeneric("incidMat")))
+  setGeneric("incidMat", function(object)
+            standardGeneric("incidMat"))
+setMethod("incidMat", "GSE", function(object)
+         object@incidMat)
+
+if (is.null(getGeneric("gTestStat")))
+  setGeneric("gTestStat", function(object)
+            standardGeneric("gTestStat"))
+setMethod("gTestStat", "GSE", function(object)
+         object@gTestStat)
+
+if (is.null(getGeneric("gsTestStat")))
+  setGeneric("gsTestStat", function(object)
+            standardGeneric("gsTestStat"))
+setMethod("gsTestStat", "GSE", function(object)
+         object@gsTestStat)
+
+if (is.null(getGeneric("expData")))
+  setGeneric("expData", function(object)
+            standardGeneric("expData"))
+setMethod("expData", "GSE", function(object)
+         object@expData)
+
+if (is.null(getGeneric("descr")))
+  setGeneric("descr", function(object)
+            standardGeneric("descr"))
+setMethod("descr", "GSE", function(object)
+         object@descr)
+
+if (is.null(getGeneric("incidMat<-")))
+  setGeneric("incidMat<-", function(object, value)
+            standardGeneric("incidMat<-"))
+setReplaceMethod("incidMat", "GSE", function(object, value)
+         {
+           object@incidMat<-value
+           object
+         }
+)
+
+if (is.null(getGeneric("gTestStat<-")))
+  setGeneric("gTestStat<-", function(object, value)
+            standardGeneric("gTestStat<-"))
+setReplaceMethod("gTestStat", "GSE", function(object, value)
+         {
+           object@gTestStat<-value
+           object
+         }
+)
+
+if (is.null(getGeneric("gsTestStat<-")))
+  setGeneric("gsTestStat<-", function(object, value)
+            standardGeneric("gsTestStat<-"))
+setReplaceMethod("gsTestStat", "GSE", function(object, value)
+         {
+           object@gsTestStat<-value
+           object
+         }
+)
+
+if (is.null(getGeneric("expData<-")))
+  setGeneric("expData<-", function(object, value)
+            standardGeneric("expData<-"))
+setReplaceMethod("expData", "GSE", function(object, value)
+         {
+           object@expData<-value
+           object
+         }
+)
+
+if (is.null(getGeneric("descr<-")))
+  setGeneric("descr<-", function(object, value)
+            standardGeneric("descr<-"))
+setReplaceMethod("descr", "GSE", function(object, value)
+         {
+           object@descr<-value
+           object
+         }
+)
 
 ##############
 # create a class for model objects
@@ -11,7 +103,8 @@ setClass("graphModel", representation(modelData="graph"), contains="gModel")
 # the data list should include an exprSet object and maybe a vector of LL ids
 setClass("exprModel", representation(modelData="exprSet"), contains="gModel")
 
-
+# added 6/28/06 to have a gene set enrichment model
+setClass("gseModel", representation(modelData="GSE"), contains="gModel")
 
 ###########
 # create a class for views
